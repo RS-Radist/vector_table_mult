9b3553c046f6684f4cea50f4e345e349bf5a9bc8
Rodion Loskutov <rodionloskutov@iMac-Rodion.local>
2018-04-15 16:25:26 +0300

    all table mult

--- vec_tb_mult.cpp
+++ vec_tb_mult.cpp
@@ -2,35 +2,56 @@
 #include <vector>
 int main()
 {
-    int row=10;
+    int row=100;
     int col=2;
-    std::cout<<"Введите число чтобы увидеть её таблицу умножения = ";
+    //std::cout<<"Введите число чтобы увидеть её таблицу умножения = ";
    
-    int number;
-    std::cin>>number;
+   
+    //std::cin>>number;
     std::cout<<std::endl;
-    
-    std::vector <std::vector<int>  > vec(row);
+    int number=1;
+    std::vector <std::vector<int> > vec(row);
     int p=1;
-    for(size_t i = 0; i < row; ++i)
-    {
-        vec[i].resize(col);
-        for (size_t j=0;j<col;++j)
+        for(size_t i = 0; i < row; ++i)
         {
-            if (j==1)
+            vec[i].resize(col);
+            for (size_t j=0;j<col;++j)
             {
-                vec[i][j]=number*p;
-                p++;
+                if (j==1) // [0] [1]
+                {
+                    vec[i][j]=number*p;
+                    p++;
+                    if(i==9||i==19||i==29||i==39||i==49||i==59||i==69||i==79||i==89||i==99)
+                    {
+                        ++number;
+                        p=1;
+                    }
+                }
+                else
+                vec[i][j]=number; // [0][0]
             }
-            else
-            vec[i][j]=number;
         }
-    }
+    
+    
     for (int i=0;i<row;++i)
     {
         for(int j=0;j<col;++j)
         {
             std::cout<<vec[i][j]<<" ";
+            if ((i==9&&j==1)||
+                (i==19&&j==1)||
+                (i==29&&j==1)||
+                (i==39&&j==1)||
+                (i==49&&j==1)||
+                (i==59&&j==1)||
+                (i==69&&j==1)||
+                (i==79&&j==1)||
+                (i==89&&j==1)||
+                (i==99&&j==1))
+            {
+                std::cout<<std::endl;
+                std::cout<<"________________________"<<std::endl;
+            }
         }
         std::cout<<std::endl;
     }
